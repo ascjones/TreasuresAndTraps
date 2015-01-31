b5aa096a66ebbbdb26451defb5513422bbba0d65
Andrew Jones <ascjones@gmail.com>
2015-01-31 21:39:13 +0000

    initial commit

---
+++ Chapter-0/Game/Brains.fs
@@ -0,0 +1,15 @@
+﻿namespace Game
+
+open System
+open Game.Domain
+
+module Brains =
+
+    let rand = new Random();
+
+    let randomDecide () =
+        match rand.Next(3) with
+        | 0 -> Straight
+        | 1 -> Left
+        | 2 -> Right
+        | x -> failwithf "Random too bif %i" x
